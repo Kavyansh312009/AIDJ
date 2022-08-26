c63d5c1517fa224e1ddb774a8a8c8460c6a5724e
Kavyansh312009 <94540332+Kavyansh312009@users.noreply.github.com>
2022-08-26 19:39:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song ="";
 song2 ="";
 
+rightWristX=0;
+rightWristY=0;
+
+leftWristX=0;
+leftWristY=0;
+
 function preload(){
     song =loadSound("BTS.mp3");
     song2 =loadSound("bTs(2)")
@@ -22,8 +28,17 @@ function modelLoaded(){
 function gotPoses(results){
     if(result.lenght > 0){
 
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+
+        leftWristX= results[0].pose.rightWrist.x;
+        leftWristY= results[0].pose.rightWrist.y;
     }
 }
 function draw(){
     image(video,0,0,600,500);
+    fill("deepskyblue");
+    stroke("dodgerblue");
+    circle(leftWristX+400,leftWristY,20);
+    circle(rightWristX,rightWristY,20);
 }
